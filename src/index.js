bb465935787c3f507afd977ad058304de88b4fc9
Blain Crawford <blaincrawford@gmail.com>
2022-02-08 17:18:26 -0500

    got image to work finally

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ function component() {
   const element = document.createElement('div');
   const name = document.createElement('h1');
   const theGamsH1 = document.createElement('h1');
+  const myIcon = new Image();
+  const gams = new Gams('THE GAMS');
 
   element.innerHTML = _.join(['Hello', 'Webpacking', 'Fools!'], ' ');
   element.classList.add('hello')
 
   name.innerText = myName('Blain');
-  let gams = new Gams('THE GAMS');
 
   theGamsH1.id = 'gams';
   theGamsH1.innerText = gams.iAm();
   
-  const myIcon = new Image();
   myIcon.src = MyImage;
-  let myDivs = {name, element, theGamsH1, myIcon};
-  return myDivs;
+  
+  return {name, element, theGamsH1, myIcon}; 
 };
 
 let domPopulator = component();
