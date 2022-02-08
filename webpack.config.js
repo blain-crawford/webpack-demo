5684dcc4e7451a3a89dff0e703d7b90c6701b03a
Blain Crawford <blaincrawford@gmail.com>
2022-02-08 14:12:28 -0500

    got watch working

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
   },
+  module: {  
+    rules: [  
+      {  
+        test: /\.js$/,  
+        exclude: /node_modules/,   
+      }  
+    ]  
+  } 
 };
