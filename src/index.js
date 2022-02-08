9e36ad525960c3bbf988aaeda7bb44998f8fc61b
Blain Crawford <blaincrawford@gmail.com>
2022-02-08 17:04:20 -0500

    running another build

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import MyImage from './album.jpg'
 function component() {
   const element = document.createElement('div');
   const name = document.createElement('h1');
-  let theGamsH1 = document.createElement('h1');
+  const theGamsH1 = document.createElement('h1');
+
   element.innerHTML = _.join(['Hello', 'Webpacking', 'Fools!'], ' ');
   element.classList.add('hello')
+
   name.innerText = myName('Blain');
   let gams = new Gams('THE GAMS');
+
   theGamsH1.id = 'gams';
   theGamsH1.innerText = gams.iAm();
+  
   const myIcon = new Image();
   myIcon.src = MyImage;
   let myDivs = {name, element, theGamsH1, myIcon};
